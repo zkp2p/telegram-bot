0495627cc6a98da15e9a8c8d02b02e8e3f0e790d
Ben <66370595+unhappyben@users.noreply.github.com>
2025-07-30 13:33:08 +0200

    Update bot.js
    Lower case the contracts in verifier mapping

--- bot.js
+++ bot.js
@@ -1070,8 +1070,8 @@ const verifierMapping = {
   '0x9a733b55a875d0db4915c6b36350b24f8ab99df5': { platform: 'venmo', isUsdOnly: true },
   '0xaa5a1b62b01781e789c900d616300717cd9a41ab': { platform: 'revolut', isUsdOnly: false },
   '0xff0149799631d7a5bde2e7ea9b306c42b3d9a9ca': { platform: 'wise', isUsdOnly: false },
-  '0x03d17E9371C858072E171276979f6B44571C5DeA': { platform: 'paypal', isUsdOnly: false },
-  '0x0dE46433bD251027f73eD8f28E01eF05DA36a2E0': { platform: 'monzo', isUsdOnly: false },
+  '0x03d17e9371c858072e171276979f6b44571c5dea': { platform: 'paypal', isUsdOnly: false },
+  '0x0de46433bd251027f73ed8f28e01ef05da36a2e0': { platform: 'monzo', isUsdOnly: false },
   '0xf2ac5be14f32cbe6a613cff8931d95460d6c33a3': { platform: 'mercado pago', isUsdOnly: false },
   '0x431a078a5029146aab239c768a615cd484519af7': { platform: 'zelle', isUsdOnly: true }
 
